44b2028a1287248ef874243985d285109d05a3dc
QuanYLiu <liuyuanquan@live.cn>
2020-08-26 11:37:20 +0800

    Initial commit from PanJiaChen/vue-admin-template

---
+++ src/lang/en.js
@@ -0,0 +1,51 @@
+export default {
+  route: {
+    dashboard: 'Dashboard',
+    errorPages: 'Error Pages',
+    page401: '401',
+    page404: '404',
+    errorLog: 'Error Log',
+    excel: 'Excel',
+    exportExcel: 'Export Excel',
+    profile: 'Profile'
+  },
+  navbar: {
+    dashboard: 'Dashboard',
+    logOut: 'Log Out',
+    profile: 'Profile'
+  },
+  login: {
+    title: 'Login Form',
+    logIn: 'Login',
+    username: 'Username',
+    password: 'Password',
+    capsLock: 'Caps lock is On',
+    usernameError: 'Please enter the correct user name',
+    passwordLenghtError: 'The password can not be less than 6 digits'
+  },
+  permission: {
+    addRole: 'New Role',
+    editPermission: 'Edit',
+    roles: 'Your roles',
+    switchRoles: 'Switch roles',
+    delete: 'Delete',
+    confirm: 'Confirm',
+    cancel: 'Cancel'
+  },
+  table: {
+    title: 'Title',
+    type: 'Type',
+    search: 'Search',
+    add: 'Add',
+    export: 'Export',
+    reviewer: 'reviewer',
+    id: 'ID',
+    date: 'Date',
+    status: 'Status',
+    actions: 'Actions',
+    edit: 'Edit',
+    delete: 'Delete',
+    cancel: 'Cancel',
+    confirm: 'Confirm'
+  }
+}
---
+++ src/lang/zh-hk.js
@@ -0,0 +1,51 @@
+export default {
+  route: {
+    dashboard: '首頁',
+    errorPages: '錯誤頁面',
+    page401: '401',
+    page404: '404',
+    errorLog: '錯誤日誌',
+    excel: 'Excel',
+    exportExcel: '匯出 Excel',
+    profile: '個人中心'
+  },
+  navbar: {
+    dashboard: '首頁',
+    logOut: '退出登錄',
+    profile: '個人中心'
+  },
+  login: {
+    title: '系統登錄',
+    logIn: '登錄',
+    username: '帳號',
+    password: '密碼',
+    capsLock: '大寫鎖定已打開',
+    usernameError: '請輸入正確的用戶名',
+    passwordLenghtError: '密碼不能少於6位數字'
+  },
+  permission: {
+    addRole: '新增角色',
+    editPermission: '編輯許可權',
+    roles: '你的許可權',
+    switchRoles: '切換許可權',
+    delete: '刪除',
+    confirm: '確定',
+    cancel: '取消'
+  },
+  table: {
+    title: '標題',
+    type: '類型',
+    search: '搜索',
+    add: '添加',
+    export: '匯出',
+    reviewer: '審核人',
+    id: '序號',
+    date: '時間',
+    status: '狀態',
+    actions: '操作',
+    edit: '編輯',
+    delete: '刪除',
+    cancel: '取 消',
+    confirm: '確 定'
+  }
+}
